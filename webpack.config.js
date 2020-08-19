3112e4537006e20aab9d4d5076be1616851dc54b
Sergey <chechelsk@gmail.com>
2020-08-19 17:13:11 +0300

    Routing implemented

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
@@ -53,7 +54,6 @@ module.exports = {
         removeComments: isProd,
         collapseWhitespace: isProd,
       },
-
     }),
     new CopyPlugin({
       patterns: [
@@ -64,6 +64,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: falename('css'),
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
   module: {
